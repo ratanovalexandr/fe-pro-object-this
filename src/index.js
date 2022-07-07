dc26e76c56f534a2379cdbb12c3629a14fa5afed
ratanovalexandr <ratanovalexandr@gmail.com>
2022-07-07 12:26:28 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-export const hotel = new Object();
+export const hotel = {
+  quantityOfPlaces: 30,
+  priceByPlace: 20,
+  bankAccount: 0,
+  guests: {
+    0: {
+      firstName: 'Bohdan',
+      lastName: 'Rammfall',
+      money: 40,
+    },
+    1: {
+      firstName: 'Bohdan',
+      lastName: 'Rammfall',
+      money: 40,
+    },
+  },
+
+  getLength: function () {
+    return Object.keys(this.guests).length;
+  },
+
+  getActualFreePlace: function () {
+    return this.quantityOfPlace - this.getLength();
+  },
+
+  paidPerPlace: function () {
+    return (this.bankAccount = this.bankAccount + this.priceByPlace);
+  },
+
+  checkInGuest: function (name, lastName, money) {
+    if (this.getActualFreePlace() === 0) {
+      return 'Sorry, we have not free spaces';
+    }
+
+    if (this.priceByPlace > money) {
+      return 'Sorry, you have not enough money';
+    }
+    const user = {
+      firstName: name,
+      lastName,
+      money: money - this.priceByPlace,
+    };
+    this.guests[this.getLength()] = user;
+
+    this.paidPerPlace();
+
+    return user;
+  },
+};
